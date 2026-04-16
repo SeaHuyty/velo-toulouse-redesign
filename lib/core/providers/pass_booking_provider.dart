8b456c6be6dee0dcc745b3d86ace2c815c351c12
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-04-16 23:33:12 +0700

    US1 – Select a Pass: Create pass booking provider state management

---
+++ lib/core/providers/pass_booking_provider.dart
@@ -0,0 +1,11 @@
+import '../../data/models/pass.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+final selectedPassProvider = StateProvider<PassModel?>((ref) => null);
+
+final purchaseDateProvider = StateProvider<DateTime?>((ref) => null);
+
+
+
+
+
